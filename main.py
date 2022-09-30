bdebb85d2d5b4924a719751394ae0f0063676003
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-09-30 17:09:32 +0000

    Added art and ANSI color codes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
+import time
 from replit import clear
+from art import logo
 
 def deal_card():
     """Returns a random card from the deck."""
@@ -18,24 +20,26 @@ def calculate_score(hand):
 
 def compare(user_score, computer_score):
     if user_score == computer_score:
-        return "Draw"
+        return "\33[33mDRAW\33[0m"
     elif computer_score == 0:
-        return "Lose, dealer has blackjack"
+        return "\33[31mLOSE\nDealer has Blackjack\33[0m"
     elif user_score == 0:
-        return "Win with blackjack"
+        return "\33[32mWIN\nBlackjack\33[0m"
     elif user_score > 21:
-        return "Lose. You went over"
+        return "\33[31mLOSE\nYou went over\33[0m"
     elif computer_score > 21:
-        return "Win. Dealer went over"
+        return "\33[32mWIN\nDealer went over\33[0m"
     elif computer_score > user_score:
-        return "Lose"
+        return "\33[31mLOSE\33[0m"
     elif user_score > computer_score:
-        return "Win"
+        return "\33[32mWIN\33[0m"
+
+play = input("Psst. Hey. Wannna play a \33[1mgame\33[0m?\n\33[32mY\33[0m/\33[31mN\33[0m ").lower()
 
 end_game = False
 while not end_game:
-    play = input("Wannna play Blackjack? Y/N ").lower()
     if play == 'y':
+        logo()
         user_cards = []
         computer_cards = []
         
@@ -43,20 +47,35 @@ while not end_game:
             user_cards.append(deal_card())
             computer_cards.append(deal_card())
 
+        clear()
+        logo()
+        print("\33[32mStarting game.\33[0m")
+        time.sleep(0.5)
+        clear()
+        logo()
+        print("\33[32mStarting game..\33[0m")
+        time.sleep(0.5)
+        clear()
+        logo()
+        print("\33[32mStarting game...\33[0m")
+        time.sleep(0.5)
+        clear()
+        logo()
+        
         stop_drawing = False
         while not stop_drawing:
             clear()
-            print("---Starting game---")
+            logo()
             
             user_score = calculate_score(user_cards)
             computer_score = calculate_score(computer_cards)
-            print(f"""Your cards: {user_cards}, current score: {user_score}
-Computer's first card: {computer_cards[0]}""")
+            print(f"""Your cards: \33[34m{user_cards}\33[0m, current score: \33[34m{user_score}\33[0m
+Computer's first card: \33[35m{computer_cards[0]}\33[0m""")
             
             if user_score == 0 or computer_score == 0 or user_score > 21:
                 stop_drawing = True
             else:
-                draw_another = input("Type 'y' to hit, type 'n' to pass: ").lower()
+                draw_another = input("\nType 'y' to hit, type 'n' to pass: ").lower()
                 if draw_another == 'y':
                     user_cards.append(deal_card())
                 else:
@@ -67,11 +86,26 @@ Computer's first card: {computer_cards[0]}""")
             computer_score = calculate_score(computer_cards)
 
         clear()
+        logo()
         
-        print(f"""Your final hand: {user_cards}, final score: {user_score}
-Computer's final hand: {computer_cards}, final score: {computer_score}\n""")
+        print(f"""Your final hand: \33[34m{user_cards}\33[0m, final score: \33[34m{user_score}\33[0m
+Computer's final hand: \33[35m{computer_cards}\33[0m, final score: \33[35m{computer_score}\33[0m\n""")
         print(compare(user_score, computer_score))
-        print("---")
+        time.sleep(1.5)
+        play = input("\nPsst. Wannna play again?\n\33[32mY\33[0m/\33[31mN\33[0m ").lower()
+
     else:
         end_game = True
-        print("---Exiting game---")
+        clear()
+        logo()
+        print("\33[31mExiting game.\33[0m")
+        time.sleep(0.5)
+        clear()
+        logo()
+        print("\33[31mExiting game..\33[0m")
+        time.sleep(0.5)
+        clear()
+        logo()
+        print("\33[31mExiting game...\33[0m")
+        time.sleep(0.5)
+        clear()
